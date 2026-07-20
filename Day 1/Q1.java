e825e2d2fbae5a58160bd6cb0bd50b774fd6ccd7
anshuman-sahu-dev <toanshumansahu@gmail.com>
2026-07-20 15:28:54 +0530

    Added Day 1 Java practice

---
+++ Day 1/Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Q1 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+
+    System.out.println("Enter your 1st number : ");
+    int num1 = sc.nextInt();
+    System.out.println("Enter your 2nd number : ");
+    int num2 = sc.nextInt();
+    System.out.println("Enter your 3rd number : ");
+    int num3 = sc.nextInt();
+
+    int sum = num1 + num2 + num3;
+    System.out.println("Sum of the numbers are : " + sum);
+
+    sc.close();
+}   
+}
